38b41f5cd258ac7ca56453462261b55b0e11c366
gaoya01 <yy890405@163.com>
2016-12-04 13:17:07 +0800

    测试

---
+++ src/HelloWord.java
@@ -0,0 +1,21 @@
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by gaoya on 2016/12/4.
+ */
+public class HelloWord extends HttpServlet{
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("测试OK");
+
+        String username=req.getParameter("username");
+        String password=req.getParameter("password");
+
+        System.out.println("用户名："+username);
+        System.out.println("密码："+password);
+    }
+}
---
+++ web/index.jsp
@@ -0,0 +1,20 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: gaoya
+  Date: 2016/12/4
+  Time: 9:56
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+  <head>
+    <title>测试tomcat</title>
+  </head>
+  <body>
+    <form action="hello" method="post">
+      <input type="text" name="username">
+      <input type=password"" name="password">
+      <input type="submit" value="提交">
+    </form>
+  </body>
+</html>
